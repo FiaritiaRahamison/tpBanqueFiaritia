3f58fb228bb9c20c2a048bc8040ebe00ba522a21
Fiaritia <70196731+Fiaritia@users.noreply.github.com>
2024-04-10 13:28:30 +0300

    Ecriture d'un bean CDI GestionnaireCompte: Déclaration de la source de données.

---
+++ src/main/java/mg/itu/fiaritia/tpbanquefiaritia/service/GestionnaireCompte.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mg.itu.fiaritia.tpbanquefiaritia.service;
+
+import jakarta.annotation.sql.DataSourceDefinition;
+import jakarta.enterprise.context.RequestScoped;
+
+/**
+ * Façade pour gérer les comptes bancaire.
+ * @author raham
+ */
+@DataSourceDefinition (
+    className="com.mysql.cj.jdbc.MysqlDataSource",
+    name="java:app/jdbc/banque",
+    serverName="localhost",
+    portNumber=3307,
+    user="root",
+    password="root", 
+    databaseName="banque",
+    properties = {
+      "useSSL=false",
+      "allowPublicKeyRetrieval=true",
+      "driverClass=com.mysql.cj.jdbc.Driver"
+    }
+)
+@RequestScoped
+public class GestionnaireCompte {
+    
+}
