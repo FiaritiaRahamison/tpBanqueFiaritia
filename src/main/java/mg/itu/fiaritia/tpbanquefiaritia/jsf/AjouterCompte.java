816431937d09484258deb225902c3eb0356c7e03
FiaritiaRahamison <fiaritia-naliarilala.rahamison@etu.unice.fr>
2024-04-14 17:02:21 +0300

    Nouvelles fonctionnalités: Enregistrement d'un nouveau compte [Tentative d'ajout des contrôles et des messages d'erreur/succès INACHEVE]

--- src/main/java/mg/itu/fiaritia/tpbanquefiaritia/jsf/AjouterCompte.java
+++ src/main/java/mg/itu/fiaritia/tpbanquefiaritia/jsf/AjouterCompte.java
@@ -4,24 +4,30 @@
  */
 package mg.itu.fiaritia.tpbanquefiaritia.jsf;
 
-import jakarta.inject.Named;
 import jakarta.enterprise.context.RequestScoped;
+import jakarta.inject.Named;
 import jakarta.inject.Inject;
+import java.io.Serializable;
 import mg.itu.fiaritia.tpbanquefiaritia.entity.CompteBancaire;
 import mg.itu.fiaritia.tpbanquefiaritia.service.GestionnaireCompte;
 import java.util.logging.Logger;
+import mg.itu.fiaritia.tpbanquefiaritia.util.Util;
 /**
  *
  * @author raham
  */
 @Named(value = "ajouterCompte")
 @RequestScoped
-public class AjouterCompte {
+public class AjouterCompte implements Serializable {
 
+    //@NotNull(message = "Le nom est obligatoire.")
     private String nom;
-    private int solde;
+    
     private static final Logger LOGGER = Logger.getLogger(CompteBancaireDetailsBean.class.getName());
 
+    //@PositiveOrZero(message = "Le solde doit être un nombre positif.")
+    private int solde;
+    
     public String getNom() {
         return nom;
     }
@@ -48,9 +54,25 @@ public class AjouterCompte {
     }
     
     public String nouveauCompte() {
+        LOGGER.info("==================NOM: "+ nom +"==============");
+        LOGGER.info("================SOLDE: "+ solde +"==============");
+        boolean erreur = false;
+        if (nom.length() == 0) {
+            LOGGER.info("==================NOM VIDE ICI==============");
+            Util.messageErreur("Aucun nom n'a été saisi !", "Aucun nom n'a été saisi!", "nouveauCompte:nom");
+            erreur = true;
+        }
+        
+        LOGGER.info("================ERREUR: "+ erreur +"==============");
+        /*if (erreur) {
+            return "listeComptes?faces-redirect=true";
+        }*/
+        
+       
         CompteBancaire compteBancaire = new CompteBancaire(nom, solde);
         LOGGER.info("Compte bancaire chargé avec succès : " + compteBancaire.toString());
         gestionnaireCompte.creerCompte(compteBancaire);
+        Util.addFlashInfoMessage("OK");
         return "listeComptes?faces-redirect=true";
     }
 }
